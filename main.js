fda04faf23b1e4f778790639721e99168bfb143c
Giseliux <giseliuxgasca@gmail.com>
2019-08-29 15:00:54 -0500

    Casi terminado

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ class Lombriz {
       this.estado=0//index
       this.saludable='si'
       //si es menor a 20 tiene hambre-necesita comida
-      this.hambre=20
+      this.hambre=30
       //acceder a los arreglos
       this.direccion='izquierda'
       
@@ -67,7 +67,7 @@ class Lombriz {
         }
     }
    moveLeft(){
-        this.x -= 10
+        this.x -= 20
         this.direccion='izquierda'
         if(this.estado<3){
           this.estado++
@@ -78,7 +78,7 @@ class Lombriz {
         }
    }
    moveRight(){
-    this.x += 10
+    this.x += 20
       this.direccion='derecha'
         
       if(this.estado<3){
@@ -90,10 +90,18 @@ class Lombriz {
         }
     }
     moveUp(){
-      this.y-=10
+      this.y-=20
     }
     moveDown(){
-      this.y+=10
+      this.y+=20
+    }
+    siChocaAyudante(obstacle) {
+      return (
+        this.x < obstacle.x + obstacle.width &&
+        this.x + this.width > obstacle.x &&
+        this.y < obstacle.y + obstacle.height &&
+        this.y + this.height > obstacle.y
+      )
     }
 }
   class Composta {
@@ -114,6 +122,22 @@ class Lombriz {
        }
   }
 
+  // class Cascarones {
+  //   constructor(x, y) {
+  //     this.x = x
+  //     this.y = y
+  //     this.width = 50
+  //     this.height = 50
+  //     this.img = new Image()
+  //     this.duracionVisibleCasc=460
+  //     this.img.src ='./assets/cascaron.png'
+  //     }
+  //     draw(){
+  //       // this.y++
+  //        ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+  //      }
+  // }
+
   class Vegetales {
     constructor(x, y,index) {
       this.arregloVegetales=new Array()
@@ -122,23 +146,36 @@ class Lombriz {
       this.width = 50
       this.height = 50
       this.index=index
-      this.duracionVisible=5
+      this.duracionVisible=350
+      this.tipo=''
 
       this.arregloVegetales[0]=new Image()
       this.arregloVegetales[1]=new Image()
       this.arregloVegetales[2]=new Image()
       this.arregloVegetales[3]=new Image()
+      this.arregloVegetales[4]=new Image()
+      this.arregloVegetales[5]=new Image()
 
       this.arregloVegetales[0].src='./assets/verdura1.png'
       this.arregloVegetales[1].src='./assets/verdura4.png'
       this.arregloVegetales[2].src='./assets/verdura5.png'
       this.arregloVegetales[3].src='./assets/verdura2.png'
+      this.arregloVegetales[4].src='./assets/cascaron.png'
+      this.arregloVegetales[5].src='./assets/gota-grande_2.gif'
 
       // this.img = new Image()
       // this.img.src ='./assets/verdura5.png'
       }
       draw() {
-        // this.y++
+        // this.y+
+         if(this.index==0 || this.index==1 || this.index==2 || this.index==3){
+           this.tipo='vegetal'
+         }else if(this.index==4){
+          this.tipo='cascaron'
+         }
+         else if(this.index==5){
+          this.tipo='humedad'
+         }
          ctx.drawImage(this.arregloVegetales[this.index], this.x, this.y, this.width, this.height)
       }
   }
@@ -150,6 +187,7 @@ class Lombriz {
       this.y = y
       this.width =88
       this.height = 51
+      this.duracionVisibleMos=460
 
       this.arregloMosca[0]=new Image()
       this.arregloMosca[1]=new Image()
@@ -186,87 +224,85 @@ class Lombriz {
   const jugador = new Lombriz(300,canvas.height-70)
   const composta = new Composta(0,canvas.height-170)
   const mosca= new Mosca(10,50)
+ // const cascaron= new Cascarones(200,50)
+  
 
   function update(){
-    let titulo= document.getElementById('tituloph')
-     titulo.innerHTML=`Nivel de pH: ${composta.ph}`
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     frames++
+    mosca.duracionVisibleMos--
+   // cascaron.duracionVisibleCasc--
     board.draw()
     jugador.draw()
     composta.draw()
+    //cascaron.draw()
     //verdura.draw()
-    mosca.draw()
-    generateVegetales()
+    if(mosca.duracionVisibleMos>0){
+      mosca.draw()
+    }
+    else {
+     setTimeout(() => {
+       mosca.y=10
+      mosca.duracionVisibleMos=460
+     },3000)
+    }
+
+    // if(cascaron.duracionVisibleCasc>0){
+    //   cascaron.draw()
+    // }
+    // else {
+    //  setTimeout(() => {
+    //   cascaron.y=generaPosicionAleatoriaY()
+    //   cascaron.x=generaPosicionAleatoriaX()
+    //   cascaron.duracionVisibleCasc=400
+    //  },2000)
+    // }
+
+
+    generateAyudantes()
+   // generaCascarones()
     checaLombriz()
     checarMosca()
-    
+    checaHumedad()
+  }
+
+  function checaHumedad(){
+    let titulo= document.getElementById('humedad')
+    titulo.innerHTML=`Nivel de humedad: ${composta.humedad}`
   }
 
      //checar si la mosca toco la composta 
   function checarMosca() {
-      if (this.isTouching(composta)){ 
-        composta.ph=composta.ph-1
+      if (mosca.isTouching(composta)){ 
+          composta.ph=composta.ph-1
        }
          console.log(composta.ph)
+       
+    let titulo= document.getElementById('tituloph')
+     titulo.innerHTML=`Nivel de pH: ${composta.ph}`
   }
 
 function checaLombriz(){
- /* arregloVegetales.forEach(element => {
-      if(girl.crashing(element)){
-          let index = arregloVegetales.indexOf(element);
-          arregloVegetales.splice(index, 1);
-          element.active = false*/
-          
-          // if(element.type == 'computadora'){
+  let titulo= document.getElementById('alimento')
+          titulo.innerHTML=`Alimento lombriz: ${jugador.hambre}`
 
-          //     points += 100
-          // }else{
-
-          //     points -= 30
-          // }
-    //  } 
-  //})
+  ayudantes.forEach(element => {
+      if(jugador.siChocaAyudante(element)){
+          let indiceAyudante = ayudantes.indexOf(element);
+          if(element.tipo == 'vegetal'){
+           jugador.hambre=jugador.hambre+1
+           ayudantes.splice(indiceAyudante, 1);
+        }
+    }
+  })
 }
-
-
-
-// function crashObject(){
-
-//   enemies.forEach(element => {
-
-//       if(girl.crashing(element)){
-
-//           let index = enemies.indexOf(element);
-
-//           enemies.splice(index, 1);
-
-//           element.active = false
-
-//           if(element.type == 'computadora'){
-
-//               points += 100
-
-//           }else{
-
-//               points -= 30
-
-//           }
-
-//       } 
-
-//   })
-
-// }
-
-
 
   function generaPosicionAleatoriaX()
   {
     const minX = 50
     const maxX = 700
     const resX= (Math.floor(Math.random() * (maxX - minX)))+10
-    console.log(resX)
+    //console.log(resX)
     return resX
   }
 
@@ -275,29 +311,42 @@ function checaLombriz(){
     const minY = 50
     const maxY = 400
     const resY= (Math.floor(Math.random() * (maxY - minY)))+15
-    console.log(resY)
+    //console.log(resY)
     return resY
   }
 
-  function generateVegetales() {
+  function generateAyudantes() {
     if (frames % 150 === 0) {
-      let randomImagen= Math.floor(Math.random() * (3 - 0))
+      let randomImagen= Math.floor(Math.random() * (6 - 0))
       ayudantes.push(new Vegetales(generaPosicionAleatoriaX(),generaPosicionAleatoriaY(),randomImagen))
     }
-     ayudantes.forEach(e => {
-      e.draw()
+      ayudantes.forEach(e => {
+       e.duracionVisible--
+       if(e.duracionVisible>0){
+         e.draw()
+       }else  if(e.duracionVisible==0){
+         let indice=ayudantes.indexOf(e)
+         ayudantes.slice(indice,1);
+       }
+     
     })
   }
 
-  // function generaMoscas(){
-  //   let randomImagenMos= Math.floor(Math.random() * (1 - 0))
-  //   const mosca=new Mosca(10,50,randomImagenMos)
-  //   console.log(mosca)
-  //   mosca.draw()
+  // function generaCascarones(){
+    
+  //   if (frames % 100 === 0) {
+  //      const cascaron= new Cascarones(generaPosicionAleatoriaX(),generaPosicionAleatoriaY())
+  //   }
+  //   if(cascaron.duracionVisibleCasc>0){
+  //     cascaron.draw()
+  //     cascaron.duracionVisibleCasc--
+  //   }
+    
   // }
 
 
   document.onkeydown = e => {
+    e.preventDefault()
     switch (e.keyCode) {
       case 37:
         jugador.moveLeft()
