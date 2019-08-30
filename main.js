4517677f21379c4ae7f04ffcc90c6907e0ecf2d7
Giseliux <giseliuxgasca@gmail.com>
2019-08-30 02:56:31 -0500

    terminado

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ const ctx = canvas.getContext('2d')
 let frames = 0
 const ayudantes = []
 const enemigos = []
+const audio=new Audio('./assets/la-atmosfera_4.mp3')
+const avanzaLombri=new Audio('./assets/mario-bros-jump.mp3')
+const comeLombri=new Audio('./assets/pacman-eating-cherry.mp3')
+const sonidoMosca=new Audio('./assets/ANIMALFLY_6049_60.mp3')
+const perdiste=new Audio('./assets/010609168_prev.mp3')
+const ganaste=new Audio('./assets/bites-ta-da-winner.mp3')
+let tiempoJuego= 0
 
 class Escenario {
     constructor() {
@@ -27,12 +34,12 @@ class Lombriz {
       this.arregloIzquierda=new Array()
       this.x = x
       this.y = y
-      this.width = 85
-      this.height = 45
+      this.width = 95
+      this.height = 55
       this.estado=0//index
       this.saludable='si'
       //si es menor a 20 tiene hambre-necesita comida
-      this.hambre=30
+      this.hambre=40
       //acceder a los arreglos
       this.direccion='izquierda'
       
@@ -122,21 +129,6 @@ class Lombriz {
        }
   }
 
-  // class Cascarones {
-  //   constructor(x, y) {
-  //     this.x = x
-  //     this.y = y
-  //     this.width = 50
-  //     this.height = 50
-  //     this.img = new Image()
-  //     this.duracionVisibleCasc=460
-  //     this.img.src ='./assets/cascaron.png'
-  //     }
-  //     draw(){
-  //       // this.y++
-  //        ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
-  //      }
-  // }
 
   class Vegetales {
     constructor(x, y,index) {
@@ -162,9 +154,6 @@ class Lombriz {
       this.arregloVegetales[3].src='./assets/verdura2.png'
       this.arregloVegetales[4].src='./assets/cascaron.png'
       this.arregloVegetales[5].src='./assets/gota-grande_2.gif'
-
-      // this.img = new Image()
-      // this.img.src ='./assets/verdura5.png'
       }
       draw() {
         // this.y+
@@ -190,21 +179,18 @@ class Lombriz {
       this.duracionVisibleMos=460
 
       this.arregloMosca[0]=new Image()
-      this.arregloMosca[1]=new Image()
       this.arregloMosca[0].src='./assets/mosca1.gif'
-      this.arregloMosca[1].src='./assets/mosca2.gif'
 
-      // this.img = new Image()
-      // this.img.src ='./assets/mosca.gif'
       }
       draw() {
+        
         if(this.y<canvas.height-100){
           this.y++
+          sonidoMosca.play()
          ctx.drawImage(this.arregloMosca[0], this.x, this.y, this.width, this.height)
-         ctx.drawImage(this.arregloMosca[1], this.x, this.y, this.width, this.height)
         }else{
+          sonidoMosca.play()
           ctx.drawImage(this.arregloMosca[0], this.x, this.y, this.width, this.height)
-          ctx.drawImage(this.arregloMosca[1], this.x, this.y, this.width, this.height)
         } 
       }
       isTouching(obstacle) {
@@ -213,9 +199,6 @@ class Lombriz {
            this.x + this.width > obstacle.x &&
            this.y < obstacle.y-197 + obstacle.height &&
            this.y  > obstacle.y
-          //this.y<canvas.height-100
-
-         // this.y
         )
       }
   }
@@ -224,78 +207,110 @@ class Lombriz {
   const jugador = new Lombriz(300,canvas.height-70)
   const composta = new Composta(0,canvas.height-170)
   const mosca= new Mosca(10,50)
- // const cascaron= new Cascarones(200,50)
   
-
   function update(){
+    audio.play()//para mandar llamar mi audio
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     frames++
     mosca.duracionVisibleMos--
-   // cascaron.duracionVisibleCasc--
+    if (frames % 30 === 0){
+      tiempoJuego++
+    }
+   
     board.draw()
     jugador.draw()
     composta.draw()
+
+    ctx.font='20px Courier'
+    //color
+    ctx.fillStyle='#000000'
+    ctx.fillText(`pH: ${composta.ph}`,90,40)
+    ctx.fillText(`vida lombriz: ${jugador.hambre}`,200,40)
+    ctx.fillText(`humedad: ${composta.humedad}%`,450,40)
+    ctx.font='30px Courier'
+
+    ctx.fillText(`Tiempo: ${tiempoJuego}`,650,40)
+  
     //cascaron.draw()
     //verdura.draw()
     if(mosca.duracionVisibleMos>0){
+     
       mosca.draw()
     }
     else {
      setTimeout(() => {
        mosca.y=10
       mosca.duracionVisibleMos=460
-     },3000)
+     },3500)
     }
-
-    // if(cascaron.duracionVisibleCasc>0){
-    //   cascaron.draw()
-    // }
-    // else {
-    //  setTimeout(() => {
-    //   cascaron.y=generaPosicionAleatoriaY()
-    //   cascaron.x=generaPosicionAleatoriaX()
-    //   cascaron.duracionVisibleCasc=400
-    //  },2000)
-    // }
-
-
     generateAyudantes()
-   // generaCascarones()
     checaLombriz()
     checarMosca()
     checaHumedad()
+    seAcaboTiempo()
+  }
+
+
+  function seAcaboTiempo(){
+    if(tiempoJuego==60){
+      clearInterval(interval)//limpio mi intervalo
+      interval=null
+       if(composta.humedad==60 && jugador.hambre>30 && composta.ph==7){
+        ctx.font='50px Courier'
+      //color
+        ctx.fillStyle='#000000'
+        ctx.fillText(`GANASTE !! `,170,150)
+        ctx.fillText(`ya sabes realizar `,100,270)
+        ctx.fillText(`lombricomposta `,100,370)
+        ganaste.play()
+      }
+      else{
+        ctx.font='50px Courier'
+      //color
+        ctx.fillStyle='#000000'
+        ctx.fillText(`Sigue intentando`,170,150)
+        ctx.fillText(` tu lombricomposta`,100,270)
+        ctx.fillText(` aún puede mejorar`,100,370)
+        perdiste.play()
+      }
+    }
   }
 
   function checaHumedad(){
-    let titulo= document.getElementById('humedad')
-    titulo.innerHTML=`Nivel de humedad: ${composta.humedad}`
+    if(composta.humedad>0 && frames % 200 === 0)
+    {
+      composta.humedad--
+      jugador.hambre--
+    }
   }
 
-     //checar si la mosca toco la composta 
+  //checar si la mosca toco la composta 
   function checarMosca() {
       if (mosca.isTouching(composta)){ 
           composta.ph=composta.ph-1
        }
-         console.log(composta.ph)
-       
-    let titulo= document.getElementById('tituloph')
-     titulo.innerHTML=`Nivel de pH: ${composta.ph}`
   }
 
 function checaLombriz(){
-  let titulo= document.getElementById('alimento')
-          titulo.innerHTML=`Alimento lombriz: ${jugador.hambre}`
 
   ayudantes.forEach(element => {
       if(jugador.siChocaAyudante(element)){
           let indiceAyudante = ayudantes.indexOf(element);
           if(element.tipo == 'vegetal'){
+            //si colisiona
+            comeLombri.play()
            jugador.hambre=jugador.hambre+1
            ayudantes.splice(indiceAyudante, 1);
         }else if(element.tipo == 'cascaron'){
-          composta.ph=composta.ph+1
+          composta.ph++
+          comeLombri.play()
           ayudantes.splice(indiceAyudante, 1);
        }
+       else if(element.tipo == 'humedad'){
+        composta.humedad++
+        comeLombri.play()
+        ayudantes.splice(indiceAyudante, 1);
+      }
     }
   })
 }
@@ -314,7 +329,6 @@ function checaLombriz(){
     const minY = 50
     const maxY = 400
     const resY= (Math.floor(Math.random() * (maxY - minY)))+15
-    //console.log(resY)
     return resY
   }
 
@@ -335,33 +349,24 @@ function checaLombriz(){
     })
   }
 
-  // function generaCascarones(){
-    
-  //   if (frames % 100 === 0) {
-  //      const cascaron= new Cascarones(generaPosicionAleatoriaX(),generaPosicionAleatoriaY())
-  //   }
-  //   if(cascaron.duracionVisibleCasc>0){
-  //     cascaron.draw()
-  //     cascaron.duracionVisibleCasc--
-  //   }
-    
-  // }
-
-
   document.onkeydown = e => {
     e.preventDefault()
     switch (e.keyCode) {
       case 37:
         jugador.moveLeft()
+        avanzaLombri.play()
         break
       case 39:
         jugador.moveRight()
+        avanzaLombri.play()
         break
       case 38:
           jugador.moveUp()
+          avanzaLombri.play()
           break;
       case 40:
           jugador.moveDown()
+          avanzaLombri.play()
           break;
       case 70:
         restart()
@@ -374,9 +379,34 @@ function checaLombriz(){
   
   window.onload = function() {
     document.getElementById("start-button").onclick = function() {
+      var div = document.getElementById("portada2");
+      div.style.display = "none"
+      var div2 = document.getElementById("portada1");
+      div2.style.display = "none"
+      var div3 = document.getElementById("portada3");
+      div3.style.display = "block"
     startGame();
     };
-  
+
+    document.getElementById("start-button2").onclick = function() {
+      var div = document.getElementById("portada2");
+      div.style.display = "none"
+      var div2 = document.getElementById("portada1");
+      div2.style.display = "none"
+      var div3 = document.getElementById("portada3");
+      div3.style.display = "block"
+    startGame();
+    };
+    
+    document.getElementById("instruc-button").onclick = function(){
+      var div = document.getElementById("portada2");
+      div.style.display = "block"
+      var div2 = document.getElementById("portada1");
+      div2.style.display = "none"
+    };
+
+
+    
    function startGame() {
     interval = setInterval(update, 1000 / 60)
     }
